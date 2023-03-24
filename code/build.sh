f1211b61735907f53474ae75d8d36bf1f780d444
cadu.goncalves <cadu.goncalves@gmail.com>
2023-03-23 21:26:00 -0300

    Added utility shell scripts

---
+++ code/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+cd $(dirname $0)
+
+get_input_plugin() {
+  local file=fluent-beats/plugins/flb-in_$2.so
+  if test -f "$file"; then
+   echo "$file"
+   return
+  fi
+  wget https://github.com/fluent-beats/fluent-bit-$1/releases/download/$3/flb-in_$2.so -q -O $file
+}
+
+download_plugins() {
+  get_input_plugin 'carbon' 'carbon' 'v1.0.0'
+  get_input_plugin 'docker-stats' 'docker_stats' 'v1.0.0'
+  get_input_plugin 'docker-info' 'docker_info' 'v1.0.0'
+  get_input_plugin 'docker-system' 'docker_system' 'v1.0.0'
+  get_input_plugin 'meminfo' 'meminfo' 'v1.0.0'
+  get_input_plugin 'load' 'load' 'v1.0.0'
+}
+
+run() {
+  docker build -f Dockerfile -t fluent-beats/fluent-beats
+}
+
+download_plugins && run
